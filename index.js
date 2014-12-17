e7d887cff284cd9f59157a9869d616bca6f07eb9
bhtz <heintz.benjamin@gmail.com>
2014-12-17 23:11:20 +0100

    exports utils

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 // Imports
 var WsApplication = require('./libs/WsApplication');
 var Hub           = require('./libs/Hub');
+var utils         = require('./libs/utils');
 
 // Exports
 exports.WsApplication = WsApplication;
 exports.Hub           = Hub;
+exports.utils         = utils;
